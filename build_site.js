18694f337dde8e817063b3f356a3f5b08d036852
Brian Zindler <zindlerb@gmail.com>
2017-03-18 16:14:24 -0700

    begin fs delete test

---
+++ build_site.js
@@ -0,0 +1,103 @@
+const fs = require('fs');
+const handlebars = require('handlebars');
+const marked = require('marked');
+const path = require('path');
+var slug = require('slug');
+
+// DELETE ALL UNPROTECTED IN ROOT
+const protectedPaths = [
+  '.git',
+  '.gitignore',
+  'README.md',
+  'build_site.js',
+  'dist',
+  'node_modules',
+  'officeHours',
+  'package.json',
+  'rain',
+  'squiggle',
+  'src',
+];
+
+fs.readdir('.', (err, files) => {
+  console.log('files', files);
+});
+
+marked.setOptions({
+  renderer: new marked.Renderer(),
+  gfm: true,
+  tables: true,
+  breaks: false,
+  pedantic: false,
+  sanitize: false,
+  smartLists: true,
+  smartypants: false
+});
+
+function getBlogPostData(blogPostName) {
+  var fileStr = fs.readFileSync('./writing/' + blogPostName, 'utf-8');
+
+  const [dataStr, markdown] = fileStr.split('~---~', 2);
+  const data = dataStr.split('\n').reduce((data, keyVal) => {
+    const [key, val] = keyVal.split(':', 2);
+    if (key && val) {
+      data[key.trim()] = val.trim();
+    }
+
+    return data;
+  }, {});
+
+  if (!markdown) {
+    throw new Error('No content in blog post');
+  }
+  data.slug = slug(data.title.toLowerCase());
+  data.markdown = marked(markdown);
+
+  return data;
+}
+
+function transformHandlebars(templateName, outputPath, data) {
+  fs.readFile('./src/templates/' + templateName + '.handlebars', 'utf-8', function(error, source){
+    if (error) {
+      throw error;
+    }
+
+    fs.writeFileSync(
+      outputPath,
+      handlebars.compile(source)(data)
+    );
+  });
+}
+
+fs.readdir('./writing', (err, files) => {
+  const blogPostData = [];
+  console.log('Loading Blog Posts');
+  files.forEach((file) => {
+    if (path.extname(file) === '.md') {
+      blogPostData.push(getBlogPostData(file));
+    }
+  });
+
+  // Build Blog Posts
+  console.log('Building Blog Posts');
+  blogPostData.forEach((blogPostDatum) => {
+    fs.access(blogPostDatum.slug, (err) => {
+      if (err.code === "ENOENT") {
+        fs.mkdirSync(blogPostDatum.slug);
+        return;
+      }
+
+      transformHandlebars(
+        'blogpost',
+        blogPostDatum,
+        path.join(blogPostDatum.slug, 'index.html')
+      );
+    });
+  });
+
+  // Build Projects Page
+  console.log('Building Projects Page');
+  transformHandlebars('projects', 'index.html', {
+    writings: blogPostData
+  });
+});
---
+++ writing/test.md
@@ -0,0 +1,9 @@
+title: Test Blog Post
+date: 2012-12-12
+description: Test testestest
+
+~---~
+
+# Title
+
+Some writing...
